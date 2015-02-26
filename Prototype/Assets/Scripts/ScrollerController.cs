43376c52a4ae13835ceb279115e9220ff45a6211
ThaliaDeShalit <thaliapenguin@gmail.com>
2015-02-26 19:24:23 +0200

    Added Bonus level
    please dont create conflicts

---
+++ Prototype/Assets/Scripts/ScrollerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollerController : MonoBehaviour {
+
+	public float speed = 0;
+	public static ScrollerController current;
+
+	float pos = 0;
+	
+	void Start () {
+		current = this;
+	}
+
+	void Update () {
+		pos += speed;
+		if (pos > 1.0f)
+			pos -= 1.0f;
+
+		renderer.material.mainTextureOffset = new Vector2(pos, 0);
+	}
+}
---
+++ Prototype/Assets/Scripts/TouchControllerBonus.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchControllerBonus : MonoBehaviour {
+
+	private PlayerMovement pm;
+
+	void Start () {
+		pm = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerMovement> ();
+	}
+
+	void FixedUpdate () {
+		for (int i = 0; i < Input.touchCount; i++) {
+			Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+			Vector2 touchPos = new Vector2(wp.x, wp.y);
+			
+			if (Input.GetTouch(i).phase == TouchPhase.Began) {
+				pm.Attack0();
+			}
+		}
+	}
+}
